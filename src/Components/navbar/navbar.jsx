7530c5b3d80a40ec1431d137ed76ae9182b89ca3
LauraSofiaEnriquezRomero <128187742+LauraSofiaEnriquezRomero@users.noreply.github.com>
2023-08-28 23:25:07 -0500

    help interface

--- src/Components/navbar/navbar.jsx
+++ src/Components/navbar/navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
 
     <nav className="navbar">
       <div>
-        <ul class="navbar-nav  mx-auto justify-content-end">
+        <ul className="navbar-nav  mx-auto justify-content-end">
 
           <li className='navbar-left'>
             <img src="https://images.unsplash.com/photo-1599305445671-ac291c95aaa9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80" className="img-logo" alt="" />
