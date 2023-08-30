982922966663c23735bbabd541d49382b76017e6
Duvan Espitia <duvan.espitia@uao.edu.co>
2023-08-30 15:17:24 -0500

    Tarjetas inicio

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/home/home';
 import Help from './Components/help/help';
 import Navbar from './Components/navbar/navbar';
-
+import React from 'react';
 function App() {
   return (
     <div className="App">
--- src/Components/home/home.jsx
+++ src/Components/home/home.jsx
@@ -6,10 +6,74 @@ import "bootstrap/dist/css/bootstrap.css"
 
 
 function Home() {
-  
+
   return (
     <div className="App">
-            <span>Home</span>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-4'>
+            <div className='target'>
+              <div className='centrar'>
+                <img className='img-target' alt='' src='https://images.unsplash.com/photo-1501183638710-841dd1904471?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'></img>
+                <p className='decrip-h'>Ubiación:</p>
+                <p className='decrip-h'>Descripción:</p>
+                <p className='decrip-h'>Precio:</p>
+              </div>
+            </div>
+          </div>
+          <div className='col-md-4'>
+            <div className='target'>
+              <div className='centrar'>
+                <img className='img-target' alt='' src='https://images.unsplash.com/photo-1501183638710-841dd1904471?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'></img>
+                <p className='decrip-h'>Ubiación:</p>
+                <p className='decrip-h'>Descripción:</p>
+                <p className='decrip-h'>Precio:</p>
+              </div>
+            </div>
+          </div>
+          <div className='col-md-4'>
+            <div className='target'>
+              <div className='centrar'>
+                <img className='img-target' alt='' src='https://images.unsplash.com/photo-1501183638710-841dd1904471?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'></img>
+                <p className='decrip-h'>Ubiación:</p>
+                <p className='decrip-h'>Descripción:</p>
+                <p className='decrip-h'>Precio:</p>
+              </div>
+            </div>
+          </div>
+          <div className='col-md-4'>
+            <div className='target'>
+              <div className='centrar'>
+                <img className='img-target' alt='' src='https://images.unsplash.com/photo-1501183638710-841dd1904471?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'></img>
+                <p className='decrip-h'>Ubiación:</p>
+                <p className='decrip-h'>Descripción:</p>
+                <p className='decrip-h'>Precio:</p>
+              </div>
+            </div>
+          </div>
+          <div className='col-md-4'>
+            <div className='target'>
+              <div className='centrar'>
+                <img className='img-target' alt='' src='https://images.unsplash.com/photo-1501183638710-841dd1904471?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'></img>
+                <p className='decrip-h'>Ubiación:</p>
+                <p className='decrip-h'>Descripción:</p>
+                <p className='decrip-h'>Precio:</p>
+              </div>
+            </div>
+          </div>
+          <div className='col-md-4'>
+            <div className='target'>
+              <div className='centrar'>
+                <img className='img-target'alt='' src='https://images.unsplash.com/photo-1501183638710-841dd1904471?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'></img>
+                <p className='decrip-h'>Ubiación:</p>
+                <p className='decrip-h'>Descripción:</p>
+                <p className='decrip-h'>Precio:</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
 
     </div>
   );
